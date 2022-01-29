58037f6607deea7dd0cda389a1ff917679038e90
joshua <krjoshua21@gmail.com>
2022-01-29 22:36:59 +0900

    6015

--- init.py
+++ init.py
@@ -3,3 +3,15 @@ print('\"!@#$%^&*()\'')
 print('\"C:\\Download\\\'hello\'.py\"')
 
 print("print(\"Hello\\nWorld\")")
+
+#6013
+# userin13a=input()
+# userin13b=input()
+# print(userin13b)
+# print(userin13a)
+
+#6014
+userin14f=float(input())
+print(userin14f)
+print(userin14f)
+print(userin14f)
