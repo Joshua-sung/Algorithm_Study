8526210bbbf7e78e7a2e1927417885c928e0a975
joshua <krjoshua21@gmail.com>
2022-01-31 18:25:26 +0900

    6019

--- init.py
+++ init.py
@@ -1,8 +1,8 @@
-print('\"!@#$%^&*()\'')
+# print('\"!@#$%^&*()\'')
 
-print('\"C:\\Download\\\'hello\'.py\"')
+# print('\"C:\\Download\\\'hello\'.py\"')
 
-print("print(\"Hello\\nWorld\")")
+# print("print(\"Hello\\nWorld\")")
 
 #6013
 # userin13a=input()
@@ -10,8 +10,25 @@ print("print(\"Hello\\nWorld\")")
 # print(userin13b)
 # print(userin13a)
 
-#6014
-userin14f=float(input())
-print(userin14f)
-print(userin14f)
-print(userin14f)
+# #6014
+# userin14f=float(input())
+# print(userin14f)
+# print(userin14f)
+# print(userin14f)
+
+# #6015
+# userin15a, userin15b= input().split(' ')
+# print(userin15a)
+# print(userin15b)
+
+#6016
+# userin16a, userin16b= input().split(' ')
+# print(userin16b+' '+userin16a)
+
+#6017
+# userin17 = input()
+# print(userin17, userin17, userin17)
+
+#6018
+# userin18a, userin18b = input().split(':')
+# print(userin18a, userin18b, sep=':')
