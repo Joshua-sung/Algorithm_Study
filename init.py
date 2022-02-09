72850030f0df929a9ae055544cf2011db1c9fa32
joshua <krjoshua21@gmail.com>
2022-02-09 20:42:52 +0900

    6044

--- init.py
+++ init.py
@@ -45,3 +45,26 @@
 # a = input()
 # n = int(a)            #입력된 a를 10진수 값으로 변환해 변수 n에 저장
 # print('%x'% n)  #n에 저장되어있는 값을 16진수(hexadecimal) 소문자 형태 문자열로 출력
+
+#6028
+# a28 = input()
+# n28 = int(a28) 
+# print('%X' % n28)  #n28에 저장되어있는 값을 16진수 대문자 형태 문자열로 출력
+
+#6029
+# a29 = input()
+# n29 = int(a29, 16)      #입력된 a29를 16진수로 인식해 변수 n에 저장
+# print('%o' % n29)  #n29에 저장되어있는 값을 8진수(octal) 형태 문자열로 출력
+
+#6030
+# n30 = ord(input())  #입력받은 문자를 10진수 유니코드 값으로 변환한 후, n에 저장한다.
+# print(n30)
+
+#6031
+# c = int(input())
+# print(chr(c))  #c에 저장되어 있는 정수 값을 유니코드 문자(chracter)로 바꿔 출력한다. 
+
+#6043
+a,b = input().split(' ')
+c=float(a)/float(b)
+print(format(c,".3f"))
