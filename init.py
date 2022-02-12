ead6b68ea72a6d7ce2b90d269f5c8a62a08b2996
joshua <krjoshua21@gmail.com>
2022-02-12 21:12:57 +0900

    6056

--- init.py
+++ init.py
@@ -42,7 +42,7 @@
 #6027
 # a = input()
 # n = int(a)            #입력된 a를 10진수 값으로 변환해 변수 n에 저장
-# print('%x'% n)  #n에 저장되어있는 값을 16진수(hexadecimal) 소문자 형태 문자열로 출력
+# print('%x'% n)  n에 저장되어있는 값을 16진수(hexadecimal) 소문자 형태 문자열로 출력
 
 #6028
 # a28 = input()
@@ -90,3 +90,11 @@
 #6048
 # a,b=input().split(' ')
 # print(int(a)<int(b)) # true가 출력됨
+
+#6052
+# n=int(input())
+# print(bool(n))
+
+#6055
+# a, b = input().split()
+# print(bool(int(a)) or bool(int(b)))
