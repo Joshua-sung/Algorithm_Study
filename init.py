a6505b94c70862a05078800697c3d66a44369dec
joshua <krjoshua21@gmail.com>
2022-02-16 20:49:55 +0900

    6064

--- init.py
+++ init.py
@@ -109,3 +109,10 @@
 #6060
 # a,b=input().split(' ')
 # print(int(a)&int(b))
+
+#6063
+a, b = input().split()
+a = int(a)  #변수 a에 저장되어있는 값을 정수로 바꾸어 다시 변수 a에 저장
+b = int(b)
+c = (a if (a>=b) else b)
+print(int(c))
