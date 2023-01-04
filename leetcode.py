ba507e9e85a0081fadcc9aa6d19717e8cefd148d
Joshua <krjoshua21@gmail.com>
2023-01-04 14:37:32 +0900

    1929

--- leetcode.py
+++ leetcode.py
@@ -22,12 +22,19 @@
 #         return address.replace('.','[.]')
 
 #2011. Final Value of Variable After Performing Operations
+# class Solution:
+#     def finalValueAfterOperations(self, operations: List[str]) -> int:
+#         answer=0
+#         for i in operations:
+#             if "++" in i:
+#                 answer+=1
+#             else:
+#                 answer-=1
+#         return answer
+
+# 1929. Concatenation of Array
 class Solution:
-    def finalValueAfterOperations(self, operations: List[str]) -> int:
-        answer=0
-        for i in operations:
-            if "++" in i:
-                answer+=1
-            else:
-                answer-=1
-        return answer
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            nums.append(nums[i])
+        return nums
